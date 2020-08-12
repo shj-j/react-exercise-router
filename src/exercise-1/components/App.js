5b3eaf5339b47932263cbffb44353337869e13de
shj-j <shang.jie@outlook.com>
2020-08-12 20:54:48 +0800

    [sj]: done ex3

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profile from './Profile';
@@ -18,13 +18,15 @@ class App extends Component {
               <Link className="top-link" to="/my-profile">My Profile</Link>
               <Link className="top-link" to="/about-us">About Us</Link>
           </div>
-          {/* <switch> */}
-            <Route path="/about-us" component={About}/>
-            <Route path="/my-profile" component={Profile}/>
+          <Switch>
+            <Route exact path="/about-us" component={About}/>
+            <Route exact path="/my-profile" component={Profile}/>
             <Route exact path="/products" component={Products}/>
-            <Route path="/products/:id" component={Product}/>
+            <Route exact path="/products/:id" component={Product}/>
             <Route exact path="/" component={Home}/>
-          {/* </switch> */}
+            <Route exact path="/goods" component={Products}/>
+            <Route path="/*" component={Home}/>
+          </Switch>
         </Router>
       </div>
     );
